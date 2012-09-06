dc7e0e4fbf3388d55b552b9ee4be478637b89fc6
Shane Kumpf <sakserv@sakserv.org>
2012-09-06 05:38:11 -0500

    Started lab 5 - Towers of Hanoi

---
+++ CSCI_131/src/com/shanekumpf/lab5/Hanoi.java
@@ -0,0 +1,21 @@
+package com.shanekumpf.lab5;
+
+import java.util.ArrayList;
+import java.lang.Math;
+
+public class Hanoi {
+
+	Move[] moves;
+	int numDisks;
+	int moveCounter;
+	
+	public Hanoi(int numDisks) {
+		this.numDisks = numDisks;
+		//this.moves = new ArrayList<Move>((int) Math.pow(2, numDisks) - 1);
+	}
+	
+	//public Move[] solve(int sourcePegNum, int destPegNum, int tempPegNum) {
+		
+	//}
+	
+}
---
+++ CSCI_131/src/com/shanekumpf/lab5/Move.java
@@ -0,0 +1,13 @@
+package com.shanekumpf.lab5;
+
+public class Move {
+
+	int startPeg;
+	int endPeg;
+	
+	public Move(int startPeg, int endPeg) {
+		this.startPeg = startPeg;
+		this.endPeg = endPeg;
+	}
+
+}
